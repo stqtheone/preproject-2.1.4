9f45b9e3e52075ee872c8870a048056291c1473f
Alexandr <gub97@mail.ru>
2020-12-14 10:11:17 +0400

    commit

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,12 @@
 package koschei.models;
 
+import koschei.KoscheiTheDeathless;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Needle7 {
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( ";
     }
 }
